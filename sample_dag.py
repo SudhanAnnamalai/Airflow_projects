773acdbe0a735cbe6973dc664036e6fbfe445393
Sudharsan A <sudharshanvas@gmail.com>
2024-01-04 17:08:41 +0530

    Update sample_dag.py

--- sample_dag.py
+++ sample_dag.py
@@ -8,19 +8,20 @@ default_args = {
     'retry_delay': timedelta(minutes=2)
 }
 
-def greet():
-  print("Hello World!")
+def greet(name, age):                                                                    #name, age as dependency parameters.
+  print(f"Hi my name is {name} \n I am {age} years Old!, \n I am about to Have a baby")
 
 
 with DAG(
   default_args = default_args,
-  dag_id = 'Python Operator Experiment',
+  dag_id = 'Baby_Callable',
   description = 'Dag to test the python Operator, and its functionalities',
-  start_date = datetime(2024, 1, 03),
+  start_date = datetime(2024, 1, 3),
   schedule_interval = "@hourly") as dag:
     task_1 = PythonOperator(
       task_id = "greet_python",
-      pythoncallable = greet
+      python_callable = greet,
+      op_kwargs = {'name': 'Vijishree', 'age': 28}
     )
 task_1
     
